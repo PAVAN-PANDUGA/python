06bf797df1e7ed0e71af12a48ba6baf1a9c95104
PAVAN-PANDUGA <pavansmarty59@gmail.com>
2025-08-01 00:22:42 +0530

    branch8

---
+++ povneg.py
@@ -0,0 +1,8 @@
+num = int(input("Enter the number of elements: "))
+
+if num > 0:
+    print("given number is positive")
+if num < 0:
+    print("given number is negative")
+if num == 0:
+    print("given number is zero")
---
+++ sumof array.py
@@ -0,0 +1,21 @@
+a = [10,20,30,40,50]
+# sum = 0
+# for i in range(len(a)):
+#     sum += a[i]
+
+# print(sum)
+
+
+# # recursivesum
+
+# def getsum(arr, n):
+#     if n == 0:
+#         return 0
+#     else:
+#         return arr[n-1] + getsum(arr, n-1)
+# arr = [10,20,40,50,80]
+
+# print(getsum(arr, len(arr)))
+
+print(sum(a))
+
